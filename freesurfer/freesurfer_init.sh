8343fc409f30560320912ea6bdb728d06fb96260
Neurodesk User <user@neurodesk.org>
2026-09-21 14:09:22 +0000

    update freesurfer init

--- freesurfer/freesurfer_init.sh
+++ freesurfer/freesurfer_init.sh
@@ -1,8 +1,8 @@
-#!/bin/bash
+#!/usr/bin/env bash
+PROFILE="$HOME/.profile"
 
-SUBJECTS_DIR="$1"
-if [ $# -lt 1 ]; then
-    SUBJECTS_DIR="$(pwd)"
+if [ -z "${SUBJECTS_DIR:-}" ]; then
+    export SUBJECTS_DIR="${1:-$(pwd)}"
 fi
 
 mkdir -p "$SUBJECTS_DIR"
@@ -14,11 +14,22 @@ export APPTAINERENV_SUBJECTS_DIR="$SUBJECTS_DIR"
 export FS_ALLOW_DEEP=1
 export APPTAINERENV_FS_ALLOW_DEEP="$FS_ALLOW_DEEP"
 
-echo "SUBJECTS_DIR=$SUBJECTS_DIR"
+if ! grep -q "alias fsaseg=" "$PROFILE" 2>/dev/null; then
+cat >> "$PROFILE" <<'EOF'
 
-echo "Setting aliases: fsaseg, fsbrainmask, fswm, fst1, fsaparc"
+# >>> FreeSurfer fsaseg alias >>>
 alias fsaseg='freeview -v mri/brainmask.mgz mri/aseg.mgz:colormap=lut:opacity=0.2 -f surf/lh.pial:edgecolor=red surf/rh.pial:edgecolor=red surf/lh.white:edgecolor=yellow surf/rh.white:edgecolor=yellow'
 alias fsbrainmask='freeview -v mri/brainmask.mgz -f surf/lh.pial:edgecolor=red surf/rh.pial:edgecolor=red surf/lh.white:edgecolor=yellow surf/rh.white:edgecolor=yellow'
 alias fswm='freeview -v mri/wm.mgz mri/brainmask.mgz -f surf/lh.pial:edgecolor=red surf/rh.pial:edgecolor=red surf/lh.white:edgecolor=yellow surf/rh.white:edgecolor=yellow'
 alias fst1='freeview -v mri/T1.mgz'
 alias fsaparc='freeview -v mri/orig.mgz mri/aparc+aseg.mgz:colormap=lut:opacity=0.4 -f surf/lh.white:annot=aparc.annot'
+# <<< FreeSurfer fsaseg alias <<<
+EOF
+
+    echo
+    echo "Aliasy fsaseg, fsbrainmask etc. zostały dodane do ~/.profile."
+    echo "Uruchom ponownie środowisko lub wykonaj:"
+    echo
+    echo "    source ~/.profile"
+    echo
+fi
